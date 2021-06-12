7e0f9f89c5789915dc133c5ed6c8896613764807
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-12 15:48:32 -0500

    commit tree c3eb69cd38421cf7a7d5fbc85470c88fb5588d8f

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import Cell from './Cell.js'
 
 export default class Matrix extends Component {
   
+  //genRow, we see that every row will map through its vals to create an array of <div className="cell"> JSX
+  //Deliverable: Once Cell componenet is made, reaplce <div className="cell"></div> with <Cell value={val} />
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    //console.log(vals) //console.logs [] and 0: "#F00"
+    return vals.map(val => <Cell value={val} />) // replace me and render a cell component instead!
   }
   
+  //genMatrix is an instance method which maps this.props.values to an array of JSX
+  //basically every 'row' in this.props.values will create a <div className="row">
   genMatrix = () => {
+    console.log(this.props.values) // console logs an array 0: (10) ["#F00", "#00F", "#F00", "#00F"
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
+  
+  //render method shows us what our Matrix component looks like:
+  //a <div> tag with id=matrix.
+  //inside div, we invoke this.genMatrix(). The function above
   
   render() {
     return (
@@ -17,5 +28,17 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
 }
+
+  // Deliverable: Make a default prop, values to determine it's cell colors 
+  //              This is a 10x10 array (10 rows, with 10 values in each row)
+  Matrix.defaultProps = {
+    values: (() => {
+      const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+      return (new Array(10).fill(defRow))
+    })()
+  }
+  
+
+//genMatix, for every array a row is created 
+//genRow, for every row, the data inside the row, a cell is created. 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
+//Matrix gets a values prop of pattern1 which is imported from data.js
+//open data.js to see that pattern1 is a nested array of '#F00' and '#00F' (red and blue)
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
